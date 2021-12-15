954db0ced4e0c3e23d994b9f83d36689a6ccac5c
Louis <clin750@aucklanduni.ac.nz>
2021-12-15 17:35:56 +1300

    feat: overlay scrollbar

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,9 +1,12 @@
 window.onscroll = () => {
-  // const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
   const vh = 100;
   if (document.body.scrollTop > vh || document.documentElement.scrollTop > vh) {
-    document.querySelector('nav').classList.add('show')
+    document.querySelector("nav").classList.add("show");
   } else {
-    document.querySelector('nav').classList.remove('show')
+    document.querySelector("nav").classList.remove("show");
   }
-}
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  OverlayScrollbars(document.querySelectorAll("body"), {});
+});
