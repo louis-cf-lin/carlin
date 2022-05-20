f434c6cdd95ae0212ffd263c112ea76b86404fc0
Louis <louiscflin@gmail.com>
2022-05-20 20:04:57 +1200

    updated prompt

--- components/Prompt/Prompt.tsx
+++ components/Prompt/Prompt.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { FC } from "react";
 import ButtonOutline from "../UI/ButtonOutline";
 import classes from "./Prompt.module.scss";
@@ -8,6 +9,9 @@ const Prompt: FC = () => {
       <h2 className={classes.title}>It&apos;s time to scale</h2>
       <div className={classes.subtitle}>We&apos;re more than just IT.</div>
       <ButtonOutline label="Get in touch" />
+      <div className={classes.bg}>
+        <Image src="/prompt.jpg" layout="fill" objectFit="cover" alt="Scale" />
+      </div>
     </div>
   );
 };
