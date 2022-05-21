564b31e547ec995edf4610ba9413fb91756b2e40
Louis <louiscflin@gmail.com>
2022-05-21 23:02:53 +1200

    partners title

--- components/Partners/Partners.tsx
+++ components/Partners/Partners.tsx
@@ -6,35 +6,48 @@ import classes from "./Partners.module.scss";
 const Partners: FC = () => {
   return (
     <div className={classes.section}>
-      <div className={classes.logo}>
-        <Image
-          src="/shuttle.png"
-          layout="fill"
-          objectFit="contain"
-          alt="Shuttle"
-        />
-      </div>
-      <div className={classes.logo}>
-        <Image
-          src="/microsoft.png"
-          layout="fill"
-          objectFit="contain"
-          alt="Microsoft"
-        />
-      </div>
-      <div className={classes.logo}>
-        <Image src="/bosch.png" layout="fill" objectFit="contain" alt="Bosch" />
-      </div>
-      <div className={classes.logo}>
-        <Image src="/intel.png" layout="fill" objectFit="contain" alt="Intel" />
-      </div>
-      <div className={classes.logo}>
-        <Image
-          src="/infortrend.png"
-          layout="fill"
-          objectFit="contain"
-          alt="Infortrend"
-        />
+      <div className={classes.title}>Proud partners</div>
+      <div className={classes.container}>
+        <div className={classes.logo}>
+          <Image
+            src="/shuttle.png"
+            layout="fill"
+            objectFit="contain"
+            alt="Shuttle"
+          />
+        </div>
+        <div className={classes.logo}>
+          <Image
+            src="/microsoft.png"
+            layout="fill"
+            objectFit="contain"
+            alt="Microsoft"
+          />
+        </div>
+        <div className={classes.logo}>
+          <Image
+            src="/bosch.png"
+            layout="fill"
+            objectFit="contain"
+            alt="Bosch"
+          />
+        </div>
+        <div className={classes.logo}>
+          <Image
+            src="/intel.png"
+            layout="fill"
+            objectFit="contain"
+            alt="Intel"
+          />
+        </div>
+        <div className={classes.logo}>
+          <Image
+            src="/infortrend.png"
+            layout="fill"
+            objectFit="contain"
+            alt="Infortrend"
+          />
+        </div>
       </div>
     </div>
   );
